d0afb2761dfa7caa0627a58ad92c1f68f9a0dd71
MaloOllivier <malo.ollivier@etudiant.univ-rennes.fr>
2026-02-06 14:46:45 +0100

    ajout verif pos

--- main.py
+++ main.py
@@ -1,19 +1,7 @@
 print("\nLE SUPER PROGRAMME DE MALO LUCAS ET GATO\n\n")
 
-def estValide(x,y) :
-    valide = True
-    if x > 8 or y > 8:
-        valide = False
-    if x < 1 or y < 1:
-        valide = False
-
-    pos = Position(x,y)
-    return valide, pos
-
-tabVisit = []
-
 class Position:
-    x, y = 0, 0 
+    x, y = 0, 0
     def __init__(self, x, y):
         self.x = x
         self.y = y
@@ -21,7 +9,29 @@ class Position:
     def __eq__(self, value):
         return self.x == value.x and self.y == value.y
 
+def estValide(x,y) :
+    if not hasattr(estValide, "positions"):
+        estValide.positions = []
+    
+    posVerif = x, y
+    dejaVu = False
+    for pos in positions:
+        if pos == posVerif:
+            dejaVu = False
+            break
+    
+    if dejaVu:
+        valide = True
+        if x > 8 or y > 8:
+            valide = False
+        if x < 1 or y < 1:
+            valide = False
+    
+    pos = Position(x,y)
+    return valide, pos
+
 
 pos = Position()
-
-valide, pos = estValide
+positions = []
+valide, pos = estValide(2, 6)
+positions.append(pos)
