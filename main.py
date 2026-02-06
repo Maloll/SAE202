19c5fd1dd2d6db7bcea5a1803cb87001cd46ca01
MaloOllivier <malo.ollivier@etudiant.univ-rennes.fr>
2026-02-06 15:09:06 +0100

    estValide fonctionnel

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print("\nLE SUPER PROGRAMME DE MALO LUCAS ET GATO\n\n")
+print("\nLE SUPER PROGRAMME DE MORTY\n\n")
 
 class Position:
     x, y = 0, 0
@@ -12,26 +12,29 @@ class Position:
 def estValide(x,y) :
     if not hasattr(estValide, "positions"):
         estValide.positions = []
-    
-    posVerif = x, y
+
+    posVerif = Position(x,y)
     dejaVu = False
-    for pos in positions:
+    for pos in estValide.positions:
         if pos == posVerif:
-            dejaVu = False
+            dejaVu = True
+            valide = False
             break
-    
-    if dejaVu:
+        
+    if not(dejaVu):
         valide = True
         if x > 8 or y > 8:
             valide = False
         if x < 1 or y < 1:
             valide = False
+
+    if valide:
+        pos = Position(x,y)
+        estValide.positions.append(pos)
     
-    pos = Position(x,y)
-    return valide, pos
+    return valide
 
 
-pos = Position()
-positions = []
-valide, pos = estValide(2, 6)
-positions.append(pos)
+print(f"2, 6 = {estValide(2, 6)}")
+print(f"5, 4 = {estValide(5, 4)}")
+print(f"2, 6 = {estValide(2, 6)}")
