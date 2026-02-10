ce36494756fffa0dda1e476aef3b5c01d20d4188
MaloOllivier <malo.ollivier@etudiant.univ-rennes.fr>
2026-02-10 16:41:34 +0100

    changement  valide pour check tableau

--- main.py
+++ main.py
@@ -1,42 +1,30 @@
 import numpy as np
 print("\nLE SUPER PROGRAMME DE MORTY\n\n")
 
-class Position:
-    x, y = 0, 0
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-    def __eq__(self, value):
-        return self.x == value.x and self.y == value.y
+global taille
+taille = 5
 
 def estValide(x,y) :
-    posVerif = Position(x,y)
-    dejaVu = False
-    for pos in positions:
-        if pos == posVerif:
-            dejaVu = True
-            valide = False
-            break
-
-    if not(dejaVu):
-        valide = True
-        if x > 7 or y > 7:
+    valide = True
+    if plateau[x][y] != 1:
+        if x > taille - 1 or y > taille - 1:
             valide = False
         if x < 1 or y < 1:
             valide = False
-
-    if valide:
-        pos = Position(x,y)
-        positions.append(pos)
-    
+    else:
+        valide = False
+        
     return valide
 
-       
-plateau = [[0 for _ in range(8)]for _ in range(8)]
+
+def backtracking():
+    plateau[1][1] = 1
+
+plateau = [[0 for _ in range(taille)]for _ in range(taille)]
 global positions
 positions = []
+backtracking()
 print(np.matrix(plateau))
-print(f"2, 6 = {estValide(2, 6)}")
-print(f"5, 4 = {estValide(5, 4)}")
-print(f"2, 6 = {estValide(2, 6)}")
+print(f"1, 1 = {estValide(1, 1)}")
+print(f"4, 2 = {estValide(4, 2)}")
+print(f"1, 1 = {estValide(1, 1)}")
