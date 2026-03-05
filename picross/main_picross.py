6c4d8b977ae192ee1f7925c0db3f86485f838692
Maloll <malo.ollivier@etudiant.univ-rennes.fr>
2026-03-05 20:12:17 +0100

    test

--- picross/main_picross.py
+++ picross/main_picross.py
@@ -6,11 +6,9 @@ indices_ligne, indices_colonne = picrossMaker("picross/table.pic")
 
 debut = time.perf_counter()
 
-for _ in range(10000000) : 
+for _ in range(100000000) : 
     est_valide([1,0,0,1,1,1],indices_ligne[0])
 print("fini")
-
-est_valide([1,0,0,1,1,1],indices_ligne[0])
 
 fin = time.perf_counter()
 print(f"Exécuté en : {(fin - debut) * 1000:.4f} ms")
