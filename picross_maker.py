0ee882b2bab82f081136d1d46b3d4708dc13fba6
Maloll <malo.ollivier@etudiant.univ-rennes.fr>
2026-03-05 17:00:55 +0100

    ajout des colonnes

--- picross_maker.py
+++ picross_maker.py
@@ -11,7 +11,7 @@ for ligne in picross :
     print(ligne)
 
 
-grille_indice = []
+grille_indice_ligne = []
 for ligne in picross :
     n = 0
     indices_ligne = []
@@ -26,5 +26,27 @@ for ligne in picross :
             indices_ligne.append(n)
             n = 0
     print(indices_ligne)
-    grille_indice.append(indices_ligne)
+    grille_indice_ligne.append(indices_ligne)
 
+
+grille_indice_colonne = []
+for j in range(len(picross)) :
+    colonne = []
+    for ligne in picross :
+        colonne.append(ligne[j])
+    n = 0
+    indices_colonne = []
+    longueur = len(colonne) - 1
+    for i, c in enumerate(colonne) :
+        if c != 0:
+            n = n + 1
+            if i == longueur:
+                indices_colonne.append(n)
+
+        elif n > 0:
+            indices_colonne.append(n)
+            n = 0
+    grille_indice_colonne.append(indices_colonne)
+
+print(f"Indices lignes : {grille_indice_ligne}")
+print(f"Indices colonnes : {grille_indice_colonne}")
