22c7125d45669c0588cbfba4a8f14a091dbf4a5a
Maloll <malo.ollivier@etudiant.univ-rennes.fr>
2026-03-06 15:26:08 +0100

    avancé sur backtraking

--- picross/backtracking_picross.py
+++ picross/backtracking_picross.py
@@ -7,11 +7,33 @@ indices_ligne, indices_colonne, picross = picrossMaker("picross/table.pic")
 taille = len(indices_ligne)
 """ commande numpy trouvé sur internet permetant la creation d'un tableau avec toutes 
     les combinaisons binaires possible pour une taille donné"""
-binary_list = np.unpackbits(np.expand_dims(np.arange(2 ** taille, dtype=np.uint8), -1), axis=1, bitorder='little', count=taille)
+binary_list = np.unpackbits(np.expand_dims(np.arange(2 ** taille, dtype=np.uint8), -1), axis=1, bitorder='little', count=taille).tolist()
+
+grille = []
 
 def backtraking_du_futur(compteur) :
     plateau = []
-    if compteur == 10 :
+    if compteur == taille * 2 :
         return True
-    for bin in binary_list :
+    
+    if compteur <= taille :
+        for bin in binary_list :
+            if est_valide(bin, indices_ligne[compteur]) :
+                if(backtraking_du_futur(compteur+1)) :
+                    grille.append(bin)
+                    return True
+    
+    elif compteur > taille :
+        colonne = []
+        for ligne in picross :
+            colonne.append(ligne[compteur - 5])
+        if est_valide(colonne, indices_colonne[compteur - 5]) :
+            if backtraking_du_futur(compteur+1) :
+                return True
+    grille.pop
+    return False
         
+
+
+print(backtraking_du_futur(0))
+print(grille[::-1])
