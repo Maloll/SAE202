c78b1de03cff0c70e505024c36ad834562fad028
Maloll <malo.ollivier@etudiant.univ-rennes.fr>
2026-03-10 17:01:42 +0100

    fix liste binaire

--- picross/backtracking_picross.py
+++ picross/backtracking_picross.py
@@ -6,7 +6,7 @@ import numpy as np
 indices_ligne, indices_colonne, picross = picrossMaker("picross/10x10.pic")
 taille = len(indices_ligne)
 print(len(indices_ligne))
-binary_list = np.unpackbits(np.expand_dims(np.arange(2 ** taille, dtype=np.uint8), -1), axis=1, bitorder='little', count=taille).tolist() #cette ligne permet la creation de toutes les combinaisons binaire données pour une certaine taille
+binary_list = np.unpackbits(np.arange(2 ** taille, dtype=np.uint32).astype('<u4').view(np.uint8).reshape(-1, 4), axis=1, bitorder='little', count=taille).tolist() #cette ligne permet la creation de toutes les combinaisons binaire données pour une certaine taille
 
 grille = []
 
