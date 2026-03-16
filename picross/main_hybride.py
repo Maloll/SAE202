b0cdf7722911d84c43d7bcd73fa0aa62eb494a12
Maloll <malo.ollivier@etudiant.univ-rennes.fr>
2026-03-16 15:23:33 +0100

    recherche des lignes_valides sur lignes et colonnes

--- picross/main_hybride.py
+++ picross/main_hybride.py
@@ -5,7 +5,7 @@ from picross_certains import cases_certaines_tab, tab_fusion
 import time
 import numpy as np
 
-indices_ligne, indices_colonne, picross = picrossMaker("picross/5x5.pic")
+indices_ligne, indices_colonne, picross = picrossMaker("picross/10x10.pic")
 taille = len(indices_ligne)
 binary_list = np.unpackbits(np.arange(2 ** taille, dtype=np.uint32).astype('<u4').view(np.uint8).reshape(-1, 4), axis=1, bitorder='little', count=taille).tolist() #cette ligne permet la creation de toutes les combinaisons binaire données pour une certaine taille
 print(f"Table picross en {len(indices_ligne)}x{len(indices_ligne)}")
@@ -35,22 +35,26 @@ def trakbacking(bl, ligne, indice):
     for bin in bl:
         if valide_hybride(bin, ligne, indice):
             tab_valide.append(bin)
-            print()
     return tab_valide
 
-def backtraking(grille, il, ic, bl):
-    tab_valide = []
-    grille_valide = []
+def tab_valides(grille, il, ic, bl, tab_valide_ligne, tab_valide_colonne, grille_valide):
     for i, ligne in enumerate(grille):
-        tab_valide.append(trakbacking(bl, ligne, il[i]))
-    print(tab_valide)
+        tab_valide_ligne.append(trakbacking(bl, ligne, il[i]))
+    for i in range(len(grille)):
+        colonne = []
+        for ligne in grille:
+            colonne.append(ligne[i])
+        tab_valide_colonne.append(trakbacking(bl,colonne,ic[i]))
 
 
 ligne_test = [0,0,1,0,0]
 indice_test = [3]
-print(trakbacking(binary_list, ligne_test, indice_test))
-# backtraking(grille, indices_ligne, indices_colonne, binary_list)
-
+#print(trakbacking(binary_list, ligne_test, indice_test))
+grille_valide = []
+tab_valide_ligne = []
+tab_valide_colonne = []
+tab_valides(grille, indices_ligne, indices_colonne, binary_list, tab_valide_ligne, tab_valide_colonne, grille_valide)
+print(f"lignes_valides_lignes : {tab_valide_ligne}\nlignes_valides_colonnes : {tab_valide_colonne}")
 print("fini")
 fin = time.perf_counter()
 print(f"Exécuté en : {(fin - debut) * 1000:.4f} ms")
